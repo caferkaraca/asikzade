a949723694869fb7ebcd2adb7569d8f6b7bc881f
caferkaraca <caferkaraca28@gmail.com>
2025-05-25 23:01:08 +0300

    v1

--- api/odeme.php
+++ api/odeme.php
@@ -1,4 +1,12 @@
 <?php
-echo "ODEME.PHP TEST SAYFASI BASARIYLA CALISTI!";
+echo "ODEME.PHP BASLADI<br>"; // 1. Mesaj
+
+require_once 'config.php';
+echo "config.php BASARIYLA DAHIL EDILDI<br>"; // 2. Mesaj
+
+include 'products_data.php';
+echo "products_data.php BASARIYLA DAHIL EDILDI<br>"; // 3. Mesaj
+
+// Hata ayıklama için şimdilik script'i burada durduralım.
 exit;
 ?>
